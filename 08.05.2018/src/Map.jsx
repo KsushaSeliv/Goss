b437ca59e0326bcc9af1d3a520039ca6205e69c2
KsushaSeliv <KsenijaSeliv@yandex.ru>
2018-06-26 02:28:08 +0300

    Create Map.jsx

---
+++ 08.05.2018/src/Map.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Map = () =>
+  <div><h1>Карта</h1><iframe title="Lomo" src="https://yandex.ru/map-widget/v1/?um=constructor%3A6324fb36b4a9f037c896de6a55d2cbd3d4d90897a12c28877a4ba291c9f482c0&amp;source=constructor" width="394" height="391" frameborder="0"></iframe></div>
+
+export default Map;  
